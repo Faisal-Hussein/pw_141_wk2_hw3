9225559619da161bf7dcd5433c61e35533844b0c
Faisal-Hussein <fh069017@ohio.edu>
2024-02-29 17:32:00 -0500

    first commit

---
+++ calculations_module.py
@@ -0,0 +1,9 @@
+from math import pi
+
+def area_calc(l, w):
+    area = l * w
+    return area
+
+def circumference_circle(radiusvalue):
+    circumference = 2 * pi * radiusvalue
+    return circumference
